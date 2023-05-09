19c03a3ce99e8a94b5cd81380a887aefe711cfca
Roel Verschuijten <roelverschuijten@gmail.com>
2023-05-09 11:48:39 +0200

    Add project files.

---
+++ Dnd.Soundboard/Client/postcss.config.js
@@ -0,0 +1,11 @@
+﻿module.exports = {
+    purge: ['./**/*.razor'],
+    darkMode: false,
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
